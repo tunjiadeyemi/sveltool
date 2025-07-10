b96dd1695da52d5c943246e0fab16f4a86888f4f
tunny17 <tvnji01@gmail.com>
2025-07-10 09:17:57 +0100

    feat: add store trigger for notepad

--- src/lib/components/Notepad.svelte
+++ src/lib/components/Notepad.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
-  import { notes } from '$lib/stores/notes';
+  import { enableNotepad, notes } from '$lib/stores/notes';
 
   export let handleClose: () => void = () => {};
 </script>
 
-<section
-  class="absolute left-[35%] top-[15%] z-[1000] mx-auto flex h-[40vh] w-[32vw] flex-col rounded-md border bg-white shadow-md"
->
-  <div class="flex items-center justify-between border-b bg-[#f2f2f2] p-2.5">
-    <p class="font-medium text-gray-600">Global Notepad</p>
+{#if $enableNotepad}
+  <section
+    class="absolute left-[35%] top-[15%] z-[1000] mx-auto flex h-[40vh] w-[32vw] flex-col rounded-md border bg-white shadow-md"
+  >
+    <div class="flex items-center justify-between border-b bg-[#f2f2f2] p-2.5">
+      <p class="font-medium text-gray-600">Global Notepad</p>
 
-    <button type="button" on:click={handleClose} class="text-2xl">&times;</button>
-  </div>
+      <button type="button" on:click={handleClose} class="text-2xl">&times;</button>
+    </div>
 
-  <section class="h-full p-2">
-    <!-- svelte-ignore element_invalid_self_closing_tag -->
-    <textarea
-      bind:value={$notes}
-      class="h-full w-full resize-none rounded-lg border border-black/30 p-2 outline-none"
-    />
+    <section class="h-full p-2">
+      <!-- svelte-ignore element_invalid_self_closing_tag -->
+      <textarea
+        bind:value={$notes}
+        class="h-full w-full resize-none rounded-lg border border-black/30 p-2 outline-none"
+      />
+    </section>
   </section>
-</section>
+{/if}
--- src/lib/stores/notes.ts
+++ src/lib/stores/notes.ts
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 
 const stored = typeof localStorage !== 'undefined' ? localStorage.getItem('sveltool-notes') : '';
 export const notes = writable(stored || '');
+export const enableNotepad = writable<boolean>(false);
